34c4f7dbf3f8ade411b5d1ef96ddb910b48d9e3e
s.fusco <s.fusco@Sfusco-NB.eustema.intranet>
2017-07-20 16:51:23 +0200

    external rest service

--- script/digident.sh
+++ script/digident.sh
@@ -28,12 +28,54 @@ PRJ_TITOLO_PROD='Digital Identity - Produzione'
 PRJ_DESCRIZIONE_PROD='Progetto Digital Identity ambiente di PRODUZIONE'
 
 # Template
-TEMPLATE_URI_DEVOPS='https://gist.githubusercontent.com/sabafusco/c135e7a1cb92c2d20dbf901eae72c413/raw/3815cb66b47c95da16c3e35e41f9961d305aca64/digident_developer_template.yaml'
-TEMPLATE_URI_DIGIDENT='https://gist.githubusercontent.com/sabafusco/c135e7a1cb92c2d20dbf901eae72c413/raw/3815cb66b47c95da16c3e35e41f9961d305aca64/digident_developer_template.yaml'
+TEMPLATE_URI_DEVOPS='https://raw.githubusercontent.com/sabafusco/Digital_Identity_openshift/master/template/devops_template.yaml'
+TEMPLATE_URI_DIGIDENT='https://raw.githubusercontent.com/sabafusco/Digital_Identity_openshift/master/template/digident_template.yaml'
 
 # Log
 LOG_FILE='openshift_script.log'
 DATA='date +%Y/%m/%d:%H:%M:%S'
+
+
+
+####################################################
+#    Parametri Template
+####################################################
+
+#Ip dei servizi rest esterni da invocare da sviluppo
+PARAM_IP_REST_ESTERNI_SVIL='192.168.13.73'
+
+#Ip dei servizi rest esterni da invocare da collaudo
+PARAM_IP_REST_ESTERNI_COLL='192.168.13.73'
+
+#Ip dei servizi rest esterni da invocare da produzione
+PARAM_IP_REST_ESTERNI_PROD='192.168.13.73'
+
+#Url html statico delle sezioni di pagina da sviluppo
+PARAM_URL_HEADER_SVIL='http://192.168.43.94/POC/html/header.html'
+PARAM_URL_FOOTER_SVIL='http://192.168.43.94/POC/html/footer.html'
+PARAM_URL_ATTI_SVIL='http://192.168.43.94/POC/html/atti.html'
+PARAM_URL_FORMAZIONE_SVIL='http://192.168.43.94/POC/html/formazione.html'
+PARAM_URL_NEWSEVENTI_SVIL='http://192.168.43.94/POC/html/newsEventi.html'
+PARAM_URL_MENUINTRANET_SVIL='http://192.168.43.94/POC/html/menuintranet.html'
+
+
+#Url html statico delle sezioni di pagina da collaudo
+PARAM_URL_HEADER_COLL='http://192.168.43.94/POC/html/header.html'
+PARAM_URL_FOOTER_COLL='http://192.168.43.94/POC/html/footer.html'
+PARAM_URL_ATTI_COLL='http://192.168.43.94/POC/html/atti.html'
+PARAM_URL_FORMAZIONE_COLL='http://192.168.43.94/POC/html/formazione.html'
+PARAM_URL_NEWSEVENTI_COLL='http://192.168.43.94/POC/html/newsEventi.html'
+PARAM_URL_MENUINTRANET_COLL='http://192.168.43.94/POC/html/menuintranet.html'
+
+
+#Url html statico delle sezioni di pagina da produzione
+PARAM_URL_HEADER_PROD='http://192.168.43.94/POC/html/header.html'
+PARAM_URL_FOOTER_PROD='http://192.168.43.94/POC/html/footer.html'
+PARAM_URL_ATTI_PROD='http://192.168.43.94/POC/html/atti.html'
+PARAM_URL_FORMAZIONE_PROD='http://192.168.43.94/POC/html/formazione.html'
+PARAM_URL_NEWSEVENTI_PROD='http://192.168.43.94/POC/html/newsEventi.html'
+PARAM_URL_MENUINTRANET_PROD='http://192.168.43.94/POC/html/menuintranet.html'
+
 
 
 ####################################################
@@ -41,28 +83,62 @@ DATA='date +%Y/%m/%d:%H:%M:%S'
 ####################################################
 
 function echo_log(){
+        #$1 messaggio da stampare
         echo `$DATA`" $1" >> $LOG_FILE
         echo `$DATA`" $1"
 }
 
 function crea_progetto(){
+        #$1 nome progetto
+        #$2 titolo progetto
+        #$3 descrizione progetto
         echo_log "Creazione progetto $1 ..."
-        oc new-project $1 --display-name="$2" --description="$3" &> $LOG_FILE
+        oc new-project $1 --display-name="$2" --description="$3" >> $LOG_FILE
 }
 
 function usa_progetto(){
+        #$1 nome progetto
         echo_log "Progetto: $1 ."
-        oc project $1 &> $LOG_FILE
+        oc project $1 >> $LOG_FILE
 }
 
 function deploy_template(){
+        #$1 uri template
+        #$2 parametri da passare al template
         echo_log "Deploy del template $1 ..."
-        oc new-app $1 &> $LOG_FILE
+        oc new-app $1 $2 >> $LOG_FILE
 }
 
-function assign_role_to_serviceaccount(){
-		echo_log "assegnazione del ruolo $1 al service account $2 ..."
-		oc policy add-role-to-user $1 $2
+function assegna_ruolo_a_serviceaccount(){
+        #$1 ruolo da assoociare
+        #$2 service account a cui associare il ruolo
+	echo_log "assegnazione del ruolo $1 al service account $2 ..."
+	oc policy add-role-to-user $1 $2
+}
+
+function attendi_condizione() {
+        #$1 nome dell'applicazione da verificare usato solo per i log
+        #$2 timeout in sec
+        #$3 comando per verificare se l'app è pronta 
+        local TIMEOUT=$(($2/5))
+        local CONDIZIONE=$3
+
+        echo_log "In attesa che $1 sia pronto..."
+        local x=1
+        
+        while [ -z "$(eval ${CONDIZIONE})" ]
+        do
+          echo_log "."
+          sleep 5
+          x=$(( $x + 1 ))
+          if [ $x -gt $TIMEOUT ]
+          then
+            echo_log "$1 non è pronto!"
+            exit 255
+          fi
+        done
+
+        echo_log "$1 è PRONTO."
 }
 
 
@@ -76,23 +152,31 @@ echo_log "****INIT SCRIPT****"
 crea_progetto "$PRJ_NOME_DEVOPS" "$PRJ_TITOLO_DEVOPS" "$PRJ_DESCRIZIONE_DEVOPS"
 usa_progetto "$PRJ_NOME_DEVOPS"
 deploy_template "$TEMPLATE_URI_DEVOPS"
+attendi_condizione "Progetto DevOps" 600 "oc get pods -n $PRJ_NOME_DEVOPS | grep 'jenkins.*'"
+sleep 5
 
 #Ambiente SVILUPPO
 crea_progetto "$PRJ_NOME_SVIL" "$PRJ_TITOLO_SVIL" "$PRJ_DESCRIZIONE_SVIL"
 usa_progetto "$PRJ_NOME_SVIL"
-assign_role_to_serviceaccount $RUOLO_SERVICE_ACCOUNT $SERVICE_ACCOUNT_JENKINS
-deploy_template "$TEMPLATE_URI_DIGIDENT"
+assegna_ruolo_a_serviceaccount $RUOLO_SERVICE_ACCOUNT $SERVICE_ACCOUNT_JENKINS
+deploy_template "$TEMPLATE_URI_DIGIDENT" "--param=IP_REST_ESTERNI=$PARAM_IP_REST_ESTERNI_SVIL --param=URL_HEADER=$PARAM_URL_HEADER_SVIL --param=URL_FOOTER=$PARAM_URL_FOOTER_SVIL --param=URL_ATTI=$PARAM_URL_ATTI_SVIL --param=URL_FORMAZIONE=$PARAM_URL_FORMAZIONE_SVIL --param=URL_NEWSEVENTI=$PARAM_URL_NEWSEVENTI_SVIL --param=URL_MENUINTRANET=$PARAM_URL_MENUINTRANET_SVIL"
+attendi_condizione "Ambiente sviluppo" 600 "oc get pods -n $PRJ_NOME_SVIL | grep 'postgres.*'"
+sleep 5
 
 #Ambiente COLLAUDO
 crea_progetto "$PRJ_NOME_COLL" "$PRJ_TITOLO_COLL" "$PRJ_DESCRIZIONE_COLL"
 usa_progetto "$PRJ_NOME_COLL"
-assign_role_to_serviceaccount $RUOLO_SERVICE_ACCOUNT $SERVICE_ACCOUNT_JENKINS
-deploy_template "$TEMPLATE_URI_DIGIDENT"
+assegna_ruolo_a_serviceaccount $RUOLO_SERVICE_ACCOUNT $SERVICE_ACCOUNT_JENKINS
+deploy_template "$TEMPLATE_URI_DIGIDENT" "--param=IP_REST_ESTERNI=$PARAM_IP_REST_ESTERNI_COLL --param=URL_HEADER=$PARAM_URL_HEADER_COLL --param=URL_FOOTER=$PARAM_URL_FOOTER_COLL --param=URL_ATTI=$PARAM_URL_ATTI_COLL --param=URL_FORMAZIONE=$PARAM_URL_FORMAZIONE_COLL --param=URL_NEWSEVENTI=$PARAM_URL_NEWSEVENTI_COLL --param=URL_MENUINTRANET=$PARAM_URL_MENUINTRANET_COLL"
+attendi_condizione "Ambiente collaudo" 600 "oc get pods -n $PRJ_NOME_COLL | grep 'postgres.*'"
+sleep 5
 
 #Ambiente PRODUZIONE
 crea_progetto "$PRJ_NOME_PROD" "$PRJ_TITOLO_PROD" "$PRJ_DESCRIZIONE_PROD"
 usa_progetto "$PRJ_NOME_PROD"
-assign_role_to_serviceaccount $RUOLO_SERVICE_ACCOUNT $SERVICE_ACCOUNT_JENKINS
-deploy_template "$TEMPLATE_URI_DIGIDENT"
+assegna_ruolo_a_serviceaccount $RUOLO_SERVICE_ACCOUNT $SERVICE_ACCOUNT_JENKINS
+deploy_template "$TEMPLATE_URI_DIGIDENT" "--param=IP_REST_ESTERNI=$PARAM_IP_REST_ESTERNI_PROD --param=URL_HEADER=$PARAM_URL_HEADER_PROD --param=URL_FOOTER=$PARAM_URL_FOOTER_PROD --param=URL_ATTI=$PARAM_URL_ATTI_PROD --param=URL_FORMAZIONE=$PARAM_URL_FORMAZIONE_PROD --param=URL_NEWSEVENTI=$PARAM_URL_NEWSEVENTI_PROD --param=URL_MENUINTRANET=$PARAM_URL_MENUINTRANET_PROD"
+attendi_condizione "Ambiente produzione" 600 "oc get pods -n $PRJ_NOME_PROD | grep 'postgres.*'"
+sleep 5
 
 echo_log "****FINE SCRIPT****"
--- script/digident_developer.sh
+++ script/digident_developer.sh
@@ -11,11 +11,29 @@ PRJ_TITOLO='Digital Identity - Developer'
 PRJ_DESCRIZIONE='Progetto per lo sviluppo dei microservizi di Digital Identity'
 
 # Template
-TEMPLATE_URI='https://gist.githubusercontent.com/sabafusco/c135e7a1cb92c2d20dbf901eae72c413/raw/3815cb66b47c95da16c3e35e41f9961d305aca64/digident_developer_template.yaml'
+TEMPLATE_URI='https://raw.githubusercontent.com/sabafusco/Digital_Identity_openshift/master/template/digident_developer_template.yaml'
 
 # Log
 LOG_FILE='openshift_script.log'
 DATA='date +%Y/%m/%d:%H:%M:%S'
+
+
+
+####################################################
+#    Parametri Template
+####################################################
+
+#Ip dei servizi rest esterni da invocare
+PARAM_IP_REST_ESTERNI='192.168.13.73'
+
+#Url html statico delle sezioni di pagina
+PARAM_URL_HEADER='http://192.168.43.94/POC/html/header.html'
+PARAM_URL_FOOTER='http://192.168.43.94/POC/html/footer.html'
+PARAM_URL_ATTI='http://192.168.43.94/POC/html/atti.html'
+PARAM_URL_FORMAZIONE='http://192.168.43.94/POC/html/formazione.html'
+PARAM_URL_NEWSEVENTI='http://192.168.43.94/POC/html/newsEventi.html'
+PARAM_URL_MENUINTRANET='http://192.168.43.94/POC/html/menuintranet.html'
+
 
 
 ####################################################
@@ -23,23 +41,30 @@ DATA='date +%Y/%m/%d:%H:%M:%S'
 ####################################################
 
 function echo_log(){
+        #$1 messaggio da stampare
         echo `$DATA`" $1" >> $LOG_FILE
         echo `$DATA`" $1"
 }
 
 function crea_progetto(){
+        #$1 nome progetto
+        #$2 titolo progetto
+        #$3 descrizione progetto
         echo_log "Creazione progetto $1 ..."
-        oc new-project $1 --display-name="$2" --description="$3" &> $LOG_FILE
+        oc new-project $1 --display-name="$2" --description="$3" >> $LOG_FILE
 }
 
 function usa_progetto(){
+        #$1 nome progetto
         echo_log "Progetto: $1 ."
-        oc project $1 &> $LOG_FILE
+        oc project $1 >> $LOG_FILE
 }
 
 function deploy_template(){
+        #$1 uri template
+        #$2 parametri da passare al template
         echo_log "Deploy del template $1 ..."
-        oc new-app $1 &> $LOG_FILE
+        oc new-app $1 $2 >> $LOG_FILE
 }
 
 
@@ -50,7 +75,12 @@ function deploy_template(){
 echo_log "****INIT SCRIPT****"
 
 crea_progetto "$PRJ_NOME" "$PRJ_TITOLO" "$PRJ_DESCRIZIONE"
+sleep 5
+
 usa_progetto "$PRJ_NOME"
-deploy_template "$TEMPLATE_URI"
+sleep 5
+
+deploy_template "$TEMPLATE_URI" "--param=IP_REST_ESTERNI=$PARAM_IP_REST_ESTERNI --param=URL_HEADER=$PARAM_URL_HEADER --param=URL_FOOTER=$PARAM_URL_FOOTER --param=URL_ATTI=$PARAM_URL_ATTI --param=URL_FORMAZIONE=$PARAM_URL_FORMAZIONE --param=URL_NEWSEVENTI=$PARAM_URL_NEWSEVENTI --param=URL_MENUINTRANET=$PARAM_URL_MENUINTRANET"
+sleep 5
 
 echo_log "****FINE SCRIPT****"
